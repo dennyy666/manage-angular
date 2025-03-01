184a65fcd7a0277065d72b1969c5e570cd92f9ea
dennyy666 <1732854785@qq.com>
2025-03-01 20:10:53 +0800

    jumpNotFound方法

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-pages',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PagesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit() {
+  }
+
+  jumpNotFound(){
+    this.router.navigateByUrl('/pages/homeManage/notFound');
   }
 
 }
