c713fd3e50b6db447380560d00beb6937a554812
dennyy666 <1732854785@qq.com>
2025-03-01 20:07:19 +0800

    添加404页面

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './PageNotFound/PageNotFound.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: 'login' },
@@ -9,6 +10,7 @@ const routes: Routes = [
     path: 'pages',
     loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule),
   },  
+  { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
