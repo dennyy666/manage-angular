f1806166cae7bec180940d0b7d555ad8ef8d9845
dennyy666 <1732854785@qq.com>
2025-05-30 21:49:50 +0800

    登录实现

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent implements OnInit {
+  validateForm: FormGroup;
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private router: Router,
+    private message: NzMessageService
+  ) {
+    this.validateForm = this.fb.group({
+      account: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {
+
+  }
+
+  submitForm() {
+    Object.values(this.validateForm.controls).forEach(control => {
+      control.markAsDirty();
+      control.updateValueAndValidity();
+    });
+    if (this.validateForm.invalid) {
+      return
+    }
+    const { account, password } = this.validateForm.value;
+    if (['姬虚空', '酒剑仙'].includes(account) && !!password) {
+      sessionStorage.setItem('user', JSON.stringify({ name: account, company: '神剑山庄' }));
+      //路由跳转
+    } else {
+      this.message.warning('用户名或者密码错误');
+    }
+  }
+
+  onceLogin(account: string) {
+    sessionStorage.setItem('user', JSON.stringify({ name: account, company: '神剑山庄' }));
+    //路由跳转
+  }
+
+  copy(account: string) {
+    navigator.clipboard.writeText(account).then(
+      () => {
+        this.message.success('复制成功');
+      },
+      () => {
+        this.message.success('复制失败');
+      },
+    );
+  }
+
+}
