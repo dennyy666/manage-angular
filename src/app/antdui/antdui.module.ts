6e5b49bacc4a7735263f0f319eb868782262e164
dennyy666 <1732854785@qq.com>
2025-06-07 12:37:58 +0800

    项目初始化

---
+++ src/app/antdui/antdui.module.ts
@@ -0,0 +1,61 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzAffixModule } from 'ng-zorro-antd/affix';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzDropDownModule } from 'ng-zorro-antd/dropdown';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzStepsModule } from 'ng-zorro-antd/steps';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzRadioModule } from 'ng-zorro-antd/radio';
+import { NzSwitchModule } from 'ng-zorro-antd/switch';
+import { NzUploadModule } from 'ng-zorro-antd/upload';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzTableModule } from 'ng-zorro-antd/table';
+import { NzToolTipModule } from 'ng-zorro-antd/tooltip';
+import { NzAlertModule } from 'ng-zorro-antd/alert';
+import { NzMessageModule } from 'ng-zorro-antd/message';
+import { NzModalModule } from 'ng-zorro-antd/modal';
+import { NzSpinModule } from 'ng-zorro-antd/spin';
+import { NzDividerModule } from 'ng-zorro-antd/divider';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    NzButtonModule,
+    NzIconModule,
+    NzTypographyModule,
+    NzGridModule,
+    NzAffixModule,
+    NzBreadCrumbModule,
+    NzDropDownModule,
+    NzMenuModule,
+    NzStepsModule,
+    NzCheckboxModule,
+    NzDatePickerModule,
+    NzFormModule,
+    NzInputModule,
+    NzRadioModule,
+    NzSwitchModule,
+    NzUploadModule,
+    NzCardModule,
+    NzTableModule,
+    NzToolTipModule,
+    NzAlertModule,
+    NzMessageModule,
+    NzModalModule,
+    NzSpinModule,
+    NzDividerModule,
+    NzSelectModule,
+  ],
+  declarations: []
+})
+export class AntduiModule { }
