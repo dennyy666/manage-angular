1ce0b65d2596c8ffbb9745654d20476200c4feef
dennyy666 <1732854785@qq.com>
2025-07-10 16:58:33 +0800

    登录静态页面实现

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, NonNullableFormBuilder, Validators } from '@angular/forms';
+import { NzMessageService } from 'ng-zorro-antd/message';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.less']
+})
+export class LoginComponent implements OnInit {
+  validateForm: FormGroup;
+  constructor(
+    private fb: NonNullableFormBuilder,
+    private message: NzMessageService,
+  ) {
+    this.validateForm = this.fb.group({
+      account: ['', [Validators.required]],
+      password: ['', [Validators.required]],
+    });
+  }
+
+  ngOnInit() {
+  }
+  submitForm() {
+    Object.values(this.validateForm.controls).forEach(control => {
+      control.markAsDirty();
+      control.updateValueAndValidity();
+    });
+    if (this.validateForm.invalid) {
+      return
+    }
+
+  }
+
+  copy(account: string) {
+    navigator.clipboard.writeText(account).then(
+      () => {
+        this.message.success('复制成功');
+      },
+      () => {
+        this.message.success('复制失败');
+      },
+    );
+  }
+}
