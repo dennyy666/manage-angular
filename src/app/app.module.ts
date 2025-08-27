e5174a76bab6140a9cb4245c7b7c2fb1a20f8db6
dennyy666 <1732854785@qq.com>
2025-08-27 20:50:48 +0800

    项目初始化

---
+++ src/app/app.module.ts
@@ -0,0 +1,35 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { zh_CN } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { provideHttpClient } from '@angular/common/http';
+import { AntduiModule } from './antdui/antdui.module';
+
+registerLocaleData(zh);
+
+@NgModule({
+  declarations: [	
+    AppComponent,
+   ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AntduiModule
+  ],
+  providers: [
+    { provide: NZ_I18N, useValue: zh_CN },
+    provideAnimationsAsync(),
+    provideHttpClient()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
