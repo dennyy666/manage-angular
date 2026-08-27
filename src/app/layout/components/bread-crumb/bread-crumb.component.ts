073084d57aafb542b7714f7156dc2cf911c9a324
dennyy666 <2751525785@qq.com>
2026-08-27 20:25:36 +0800

    后台管理主布局实现

--- src/app/layout/components/bread-crumb/bread-crumb.component.ts
+++ src/app/layout/components/bread-crumb/bread-crumb.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'app-bread-crumb',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadCrumbComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+        private router: Router,
+        private modal: NzModalService,
+        private message: NzMessageService,
+  ) { }
 
   ngOnInit() {
   }
 
+  toggle() {
+    
+  }  
+
+  openGithub() {
+    window.open("https://github.com/dennyy666/manage-angular", "_blank")
+  }
+
+  loginOut() {
+    this.modal.confirm({ 
+      nzTitle: '您确定要退出Angular admin', 
+      nzOkText: '是',
+      nzOkType: 'primary',
+      nzOkDanger: true,
+      nzOnOk: () => {
+        this.router.navigateByUrl('login');
+        this.message.success('退出登录成功');
+      },
+      nzCancelText: '否',
+      nzOnCancel: () => {
+
+      }      
+    })
+  }
 }
--- src/app/layout/components/slide-bar/slide-bar.component.ts
+++ src/app/layout/components/slide-bar/slide-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-slide-bar',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SlideBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+        private router: Router,
+  ) { }
 
   ngOnInit() {
+  }
+
+  baidu() {
+    window.open("https://www.baidu.com", "_blank")
+  }
+
+  openGithub() {
+    window.open("https://github.com/dennyy666/manage-angular", "_blank")
+  }
+
+  jumpNotFound() {
+    this.router.navigateByUrl('/layout/homeManage/notFound');
   }
 
 }
